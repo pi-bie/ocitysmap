a9e95b31b3b7e1248d41ea85cf53395431c252ce
Hartmut Holzgraefe <hartmut@php.net>
2017-08-23 20:09:42 +0000

    fixed paper format filter after changing Din names in master list
    TODO: have separate lists instead of this hack

--- ocitysmap/layoutlib/multi_page_renderer.py
+++ ocitysmap/layoutlib/multi_page_renderer.py
@@ -773,17 +773,18 @@ class MultiPageRenderer(Renderer):
     # In multi-page mode, we only accept A4, A5 and US letter as paper
     # sizes. The goal is to render booklets, not posters.
     # The default paper size is A4 portrait
+    # TODO make separate format lists so that things don't break again on name changes
     @staticmethod
     def get_compatible_paper_sizes(bounding_box,
                                    scale=Renderer.DEFAULT_MULTIPAGE_SCALE,
                                    index_position=None, hsplit=1, vsplit=1):
         valid_sizes = []
-        acceptable_formats = [ 'A5', 'A4', 'US letter' ]
+        acceptable_formats = [ 'Din A5', 'Din A4', 'US letter' ]
         for sz in ocitysmap.layoutlib.PAPER_SIZES:
             # Skip unsupported paper formats
             if sz[0] not in acceptable_formats:
                 continue
-            valid_sizes.append((sz[0], sz[1], sz[2], True, True, sz[0] == 'A4'))
+            valid_sizes.append((sz[0], sz[1], sz[2], True, True, sz[0] == 'Din A4'))
         return valid_sizes
 
     @classmethod
