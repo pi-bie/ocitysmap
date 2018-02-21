52ea9d2b991805bab371d8402519ccf44ea5da27
Hartmut Holzgraefe <hartmut@php.net>
2018-02-21 08:06:16 +0000

    do not bail out on missing properties

--- ocitysmap/umap_utils.py
+++ ocitysmap/umap_utils.py
@@ -32,26 +32,30 @@ def umap_preprocess(umap_file):
         for feature in layer['features']:
             new_props = {}
             for prop in ['name', 'color', 'opacity', 'fillColor', 'fillOpacity', 'weight', 'fill', 'stroke']:
-                if prop in feature['properties']:
-                    new_props[prop] = feature['properties'][prop]
-                elif prop in feature['properties']['_storage_options']:
-                    new_props[prop] = feature['properties']['_storage_options'][prop]
-                else:
-                    new_props[prop] = umap_defaults[prop]
+                new_props[prop] = umap_defaults[prop]
+                try:
+                    if prop in feature['properties']:
+                        new_props[prop] = feature['properties'][prop]
+                    elif prop in feature['properties']['_storage_options']:
+                        new_props[prop] = feature['properties']['_storage_options'][prop]
+                except:
+                    pass
 
             if feature['geometry']['type'] == 'Point':
                 for prop in ['iconClass', 'iconUrl']:
-                    if prop in feature['properties']['_storage_options']:
-                        if prop == 'iconUrl':
-                            val = feature['properties']['_storage_options'][prop]
-                            m = re.match(r'/uploads/pictogram/(.*)-24.*png', val)
-                            if m:
-                                val = m.group(1)
-                            new_props[prop] = val    
-                        else:
-                            new_props[prop] = feature['properties']['_storage_options'][prop]
-                    else:
-                        new_props[prop] = umap_defaults[prop]
+                    new_props[prop] = umap_defaults[prop]
+                    try:
+                        if prop in feature['properties']['_storage_options']:
+                            if prop == 'iconUrl':
+                                val = feature['properties']['_storage_options'][prop]
+                                m = re.match(r'/uploads/pictogram/(.*)-24.*png', val)
+                                if m:
+                                    val = m.group(1)
+                                    new_props[prop] = val
+                                else:
+                                    new_props[prop] = feature['properties']['_storage_options'][prop]
+                    except:
+                        pass
 
             new_props['weight'] = float(new_props['weight']) / 4
                         
