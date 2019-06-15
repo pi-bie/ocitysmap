8aaeadfd4fa2ce5be550693bcb15390fb0186837
Hartmut Holzgraefe <hartmut@php.net>
2019-06-15 11:17:19 +0000

    UTM grid refactoring, and not rendering grid when crossing a zone boundary for now

--- ocitysmap/layoutlib/render_plugins/utm_grid/__init__.py
+++ ocitysmap/layoutlib/render_plugins/utm_grid/__init__.py
@@ -15,16 +15,43 @@ def render(renderer, ctx):
     def pt2px(dot):
         return dot * renderer.dpi / 72.0
 
-    LOG.warning("-----------------------")
+    def grid_line(lat1, lon1, lat2, lon2):
+        (x1, y1) = renderer._latlon2xy(lat1, lon1)
+        (x2, y2) = renderer._latlon2xy(lat2, lon2)
+
+        ctx.save()
+        ctx.set_source_rgba(0, 0, 1.0, 0.5)
+        ctx.set_line_width(pt2px(1))
+        ctx.move_to(x1, y1)
+        ctx.line_to(x2, y2)
+        ctx.stroke()
+        ctx.restore()
+
+    def utm_zonefield2epsg(number, letter):
+        if letter.upper() >= 'N':
+            return '326%02d' % number
+        else:
+            return '327%02d' % number
+
+    def utm_zonefield2proj(number, letter):
+        if letter.upper() <= 'M':
+            south = '+south '
+        else:
+            south = ''
+
+        return '+proj=utm +zone=%d %s +ellps=WGS84 +datum=WGS84 +units=m +no_defs' % (number, south)
 
     bbox = renderer._map_canvas.get_actual_bounding_box()
 
-    (lat, lon) = bbox.get_top_left()
-    (west, north, zone_number, zone_letter) = utm.from_latlon(lat, lon)
-    LOG.warning("UTM #1 %f %f" % (west, north))
-    (lat, lon) = bbox.get_bottom_right()
-    (east, south, zone_number, zone_letter) = utm.from_latlon(lat, lon)
-    LOG.warning("UTM #2 %f %f" % (east, south))
+    (lat1, lon1) = bbox.get_top_left()
+    (west, north, zone1_number, zone1_letter) = utm.from_latlon(lat1, lon1)
+
+    (lat2, lon2) = bbox.get_bottom_right()
+    (east, south, zone2_number, zone2_letter) = utm.from_latlon(lat2, lon2)
+
+    if zone1_number != zone2_number or math.copysign(1, lat1) != math.copysign(1, lat2):
+        LOG.warning('UTM Zone mismatch - skipping UTM grid rendering for now')
+        return
 
     w_km = int(west/1000)
     e_km = int(east/1000)
@@ -32,38 +59,13 @@ def render(renderer, ctx):
     s_km = int(south/1000)
 
     for v in range(w_km - 1, e_km + 1):
-        LOG.warning("vertical line %d %d-%d", v, n_km + 1, s_km - 1)
-        (lat1, lon1) = utm.to_latlon(v * 1000, (n_km + 1) * 1000, zone_number, zone_letter)
-        (lat2, lon2) = utm.to_latlon(v * 1000, (s_km - 1) * 1000, zone_number, zone_letter)
+        (lat1, lon1) = utm.to_latlon(v * 1000, (n_km + 1) * 1000, zone1_number, zone1_letter)
+        (lat2, lon2) = utm.to_latlon(v * 1000, (s_km - 1) * 1000, zone1_number, zone1_letter)
 
-        (x1, y1) = renderer._latlon2xy(lat1, lon1)
-        (x2, y2) = renderer._latlon2xy(lat2, lon2)
+        grid_line(lat1, lon1, lat2, lon2)
 
-        ctx.save()
-        ctx.set_source_rgba(0, 0, 1.0, 0.5)
-        ctx.set_line_width(pt2px(1))
-        ctx.move_to(x1, y1)
-        ctx.line_to(x2, y2)
-        ctx.stroke()
-        ctx.restore()
-        
-
-        
     for h in range(s_km - 1, n_km + 1):
-        LOG.warning("horizontal line %d %d-%d", h, e_km, w_km)
-        (lat1, lon1) = utm.to_latlon((w_km - 1) * 1000, h * 1000, zone_number, zone_letter)
-        (lat2, lon2) = utm.to_latlon((e_km + 1) * 1000, h * 1000, zone_number, zone_letter)
+        (lat1, lon1) = utm.to_latlon((w_km - 1) * 1000, h * 1000, zone1_number, zone1_letter)
+        (lat2, lon2) = utm.to_latlon((e_km + 1) * 1000, h * 1000, zone1_number, zone1_letter)
 
-        (x1, y1) = renderer._latlon2xy(lat1, lon1)
-        (x2, y2) = renderer._latlon2xy(lat2, lon2)
-
-        ctx.save()
-        ctx.set_source_rgba(0, 0, 1.0, 0.5)
-        ctx.set_line_width(pt2px(1))
-        ctx.move_to(x1, y1)
-        ctx.line_to(x2, y2)
-        ctx.stroke()
-        ctx.restore()
-
-
-    LOG.warning("-----------------------")
+        grid_line(lat1, lon1, lat2, lon2)
