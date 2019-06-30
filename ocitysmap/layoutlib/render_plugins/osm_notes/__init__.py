49b47fec4b5b2d40b2da5fc536a63cc025bd01e7
Hartmut Holzgraefe <hartmut@php.net>
2019-06-30 21:06:53 +0000

    prevent incomplete rendering / render errors on multipage output

--- ocitysmap/layoutlib/render_plugins/osm_notes/__init__.py
+++ ocitysmap/layoutlib/render_plugins/osm_notes/__init__.py
@@ -22,6 +22,9 @@ import logging
 LOG = logging.getLogger('ocitysmap')
 
 def render(renderer, ctx):
+    if not hasattr(renderer, 'street_index'):
+        return
+
     bbox = renderer._map_canvas.get_actual_bounding_box()
     url  = ("https://api.openstreetmap.org/api/0.6/notes.json?closed=0&bbox=%f,%f,%f,%f"
             % (bbox.get_left(), bbox.get_bottom(), bbox.get_right(), bbox.get_top()))
@@ -48,7 +51,7 @@ def render(renderer, ctx):
         return 
 
     index_items = []
-    
+
     n = 0
     for note in notes['features']:
         n = n + 1
@@ -58,7 +61,7 @@ def render(renderer, ctx):
         point = Point(lat, lon)
 
         index_text = "Note %d - %s" % (n, note['properties']['comments'][0]['text'])
-        
+
         index_items.append(StreetIndexItem(index_text[0:50], point, point, None))
 
         renderer._marker('red', str(n), lat, lon, ctx, renderer.dpi)
