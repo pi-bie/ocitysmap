05a791f4e79fcd235dca02e0cabca9284b427639
Hartmut Holzgraefe <hartmut@php.net>
2022-09-17 12:05:43 +0000

    add indexer registration

---
+++ ocitysmap/indexlib/indexers.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from ocitysmap.indexlib.StreetIndex import StreetIndex
+from ocitysmap.indexlib.HealthIndex import HealthIndex
+from ocitysmap.indexlib.NotesIndex import NotesIndex
+
+_INDEXERS = [
+    StreetIndex,
+    HealthIndex,
+    NotesIndex,
+    # do not include special PoiIndex or generic GeneralIndex here!
+    ]
+
+def get_indexer_class_by_name(name):
+    """Retrieves an indexer class, by name."""
+    for indexer in _INDEXERS:
+        if indexer.name == name:
+            return indexer
+    raise LookupError('The requested indexer %s was not found!' % name)
+
+def get_indexers():
+    """Returns the list of available indexers' names."""
+    return _INDEXERS
+
+
