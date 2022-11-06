8abbf82a1e7932bceeec8365926cdbb85f800757
Hartmut Holzgraefe <hartmut@php.net>
2022-11-06 13:42:55 +0000

    check all python files for gettext strings, not just selected ones

--- i18n.py
+++ i18n.py
@@ -27,19 +27,19 @@ import sys
 import shutil
 import optparse
 import subprocess
+import glob
 
 def make_pot():
     print("Make locale/ocitysmap.pot")
     # TODO auto-generate source file list
-    subprocess.check_call(['xgettext', '-o', 'ocitysmap.pot', '-p', 'locale',
-                           '-L', 'Python',
-                           'ocitysmap/indexlib/StreetIndex.py',
-                           'ocitysmap/indexlib/HealthIndex.py',
-                           'ocitysmap/indexlib/NotesIndex.py',
-                           'ocitysmap/indexlib/TreeIndex.py',
-                           'ocitysmap/layoutlib/multi_page_renderer.py',
-                           'ocitysmap/layoutlib/single_page_renderers.py',
-                           ])
+    cmd = ['xgettext',
+           '--output=ocitysmap.pot',
+           '--output-dir=locale',
+           '--language=Python',
+           '--from-code=UTF-8',
+    ]
+    cmd += glob.glob("./**/*.py", recursive = True)
+    subprocess.check_call(cmd)
     return
 
 def make_po(languages):
